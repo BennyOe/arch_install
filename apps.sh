82bafbbdd41bf95dc010fccc901ba0642ae97cda
BennyOe <45036971+BennyOe@users.noreply.github.com>
2021-04-17 21:22:23 +0200

    Update apps.sh

--- apps.sh
+++ apps.sh
@@ -16,4 +16,4 @@ bootstrapper_dialog --title "Welcome" --msgbox "Welcome to the Apps Installation
 appfolder=$(dialog --stdout --inputbox "Enter additional apps" 0 0) || exit 1
 clear
 
-echo $appfolder
+yay -S $appfolder
