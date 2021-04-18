ce188129304985d52b82958da99e99bc47ee5258
BennyOe <45036971+BennyOe@users.noreply.github.com>
2021-04-18 22:16:08 +0200

    Update apps.sh

--- apps.sh
+++ apps.sh
@@ -43,4 +43,10 @@ yay -S --noconfirm zsh-theme-powerlevel10k-git
 echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 
 # lsd
-pacman -S lsd
+pacman -S --noconfirm lsd
+
+# zsh autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# zsh syntax highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
