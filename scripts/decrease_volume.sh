628fa856c54eddde48a8f7dc1cff4c90f6c78dbb
BennyOe <45036971+BennyOe@users.noreply.github.com>
2021-04-19 17:26:03 +0200

    Add files via upload

---
+++ scripts/decrease_volume.sh
@@ -0,0 +1,5 @@
+$ cat .scripts/decrease_volume.sh 
+#!/bin/sh
+sink=`pactl list short sinks | grep RUNNING | cut -f1`                                                                                                                    
+notify-send "Volume -5%" -t 500
+/usr/bin/pactl set-sink-volume $sink '-5%'
