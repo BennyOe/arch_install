bf31deb17ee663be80d0eb7213b7a27ff4e7e01a
BennyOe <45036971+BennyOe@users.noreply.github.com>
2021-04-20 22:50:41 +0200

    Update apps.sh

--- apps.sh
+++ apps.sh
@@ -97,55 +97,9 @@ git clone https://github.com/BennyOe/initial_dotfiles ~/.dotfiles
 ##################
 #### Symlinks ####
 ##################
-clear
-printf "Setting symlinks\n"
-sleep 2
+chmod +x ~/.dotfiles/setsymlinks.sh
+sh ~/.dotfiles/symlinks.sh
 
-cd ~/.dotfiles
-
-rm ~/.zshrc
-ln -sv ~/.dotfiles/.zshrc ~ 
-
-rm ~/.vimrc
-ln -sv ~/.dotfiles/.vimrc ~
-
-rm ~/.dwm/autostart.sh
-chmod +x ~/.dotfiles/autostart.sh
-ln -sv ~/.dotfiles/autostart.sh ~/.dwm/
-
-mkdir ~/.config/dunst
-rm ~/.config/dunst/dunstrc
-ln -sv ~/.dotfiles/dunstrc ~/.config/dunst/ 
-
-sudo ln -sv ~/.dotfiles/dwm.desktop /usr/share/xsessions
-
-mkdir ~/.config/picom/
-rm ~/.config/picom/picom.conf
-ln -sv ~/.dotfiles/picom.conf ~/.config/picom/ 
-
-rm -rf ~/.config/nvim
-mkdir ~/.config/nvim
-ln -sv ~/.dotfiles/nvim/coc-settings.json ~/.config/nvim/ 
-ln -sv ~/.dotfiles/nvim/init.vim ~/.config/nvim/ 
-
-rm -rf ~/.config/ranger
-mkdir ~/.config/ranger
-ln -sv ~/.dotfiles/ranger/rc.conf ~/.config/ranger/ 
-ln -sv ~/.dotfiles/ranger/rifle.conf ~/.config/ranger/ 
-
-rm -rf ~/.config/rofi
-mkdir ~/.config/rofi
-mkdir ~/.config/rofi/themes
-ln -sv ~/.dotfiles/rofi/config ~/.config/rofi/ 
-ln -sv ~/.dotfiles/rofi/themes/onedark.rasi ~/.config/rofi/themes/ 
-
-mkdir ~/.local/bin
-ln -sv ~/.dotfiles/scripts/pdfshrink ~/.local/bin/ 
-chmod +x ~/.dotfiles/scripts/pdfshrink
-ln -sv ~/.dotfiles/scripts/launchspt ~/.local/bin/ 
-chmod +x ~/.dotfiles/scripts/launchspt
-ln -sv ~/.dotfiles/scripts/rofi-power-menu ~/.local/bin/ 
-chmod +x ~/.dotfiles/scripts/rofi-power-menu
 
 clear
 printf "Installation finished successfully\n"
