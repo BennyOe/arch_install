98bf7326f93b1f402cd3bd31b5fad30a5d36f8c0
BennyOe <45036971+BennyOe@users.noreply.github.com>
2021-04-20 16:01:15 +0200

    Update apps.sh

--- apps.sh
+++ apps.sh
@@ -12,7 +12,9 @@ bootstrapper_dialog --title "Welcome" --msgbox "Welcome to the Apps Installation
 ##################
 ### User Input ###
 ##################
-
+clear
+printf "Installing user apps"
+sleep 2
 # app folder
 appfolder=$(dialog --stdout --inputbox "Enter additional apps" 0 0) || exit 1
 clear
@@ -22,18 +24,26 @@ yay -S --noconfirm $appfolder
 ##################
 ## Standard Apps #
 ##################
-
+clear
+printf "Installing standard apps"
+sleep 2
 yay -S --noconfirm pulseaudio pulseaudio-alsa pavucontrol pa-applet-git ponymix signal-desktop discord_arch_electron brave-bin ranger redshift flameshot autorandr mailspring whatsapp-for-linux thunar xidlehook numlockx intellij-idea-ultimate-edition zathura htop vlc neofetch
 
 ##################
 ## Lightdm #######
 ##################
+clear
+printf "Installing lightdm"
+sleep 2
 
 yay -S --noconfirm lightdm lightdm-mini-greeter
 
 ##################
 ###### ZSH #######
 ##################
+clear
+printf "Installing zsh"
+sleep 2
 
 # oh my zsh
 yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -54,10 +64,70 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 ##################
 ###### VIM #######
 ##################
+clear
+printf "Installing vim"
+sleep 2
+
 yay -S --noconfirm neovim-nightly-bin
 
 ##################
 ##### Spotify ####
 ##################
+clear
+printf "Installing spotify-tui"
+sleep 2
+
 yay -S --noconfirm spotifyd spotify-tui
+
+##################
+#### Dot Files ###
+##################
+clear
+printf "Installing dot files"
+sleep 2
+
+git clone https://github.com/BennyOe/initial_dotfiles ~/.dotfiles
+
+##################
+#### Symlinks ####
+##################
+clear
+printf "Setting symlinks"
+sleep 2
+
+cd ~/.dotfiles
+
+ln -sv ~/.dotfiles/.zshrc ~ 
+ln -sv ~/.dotfiles/.vimrc ~ 
+ln -sv ~/.dotfiles/.zshrc ~
+ln -sv ~/.dotfiles/autostart.sh ~/.dwm/
+mkdir ~/.config/dunst
+ln -sv ~/.dotfiles/dunstrc ~/.config/dunst/ 
+sudo ln -sv ~/.dotfiles/dwm.desktop /usr/share/xsessions
+mkdir ~/.config/picom
+ln -sv ~/.dotfiles/picom.conf ~/.config/picom/ 
+mkdir ~/.config/nvim
+ln -sv ~/.dotfiles/nvim/coc-settings.json ~/.config/nvim/ 
+ln -sv ~/.dotfiles/nvim/init.vim ~/.config/nvim/ 
+mkdir ~/.config/ranger
+ln -sv ~/.dotfiles/ranger/rc.conf ~/.config/ranger/ 
+ln -sv ~/.dotfiles/ranger/rifle.conf ~/.config/ranger/ 
+mkdir ~/.config/rofi
+mkdir ~/.config/rofi/themes
+ln -sv ~/.dotfiles/rofi/config ~/.config/rofi/ 
+ln -sv ~/.dotfiles/rofi/themes/onedark.rasi ~/.config/rofi/themes/ 
+
+ln -sv ~/.dotfiles/scripts/pdfshrink ~/.local/bin/ 
+ln -sv ~/.dotfiles/scripts/launchspt ~/.local/bin/ 
+ln -sv ~/.dotfiles/scripts/rofi-power-menu ~/.local/bin/ 
+
+clear
+printf "Installation finished."
+printf "rebooting the system."
+printf "press a key to continue...\n"
+read
+reboot
+
+
+
 
