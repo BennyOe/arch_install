4cbfc863630e181ba48767a5f25ed5fa9956e1c1
BennyOe <45036971+BennyOe@users.noreply.github.com>
2021-04-23 23:25:43 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -203,7 +203,7 @@ clear
 printf "beginning with Arch installation\n"
 sleep 5
 
-pacstrap /mnt base linux linux-firmware base-devel vim networkmanager git man bash
+pacstrap /mnt base linux linux-firmware base-devel vim networkmanager git man bash  wireless_tools wpa_supplicant dialog
 
 #configure the system
 printf "setting fstab\n"
