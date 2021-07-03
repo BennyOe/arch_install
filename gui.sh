99abfc0f2ab4e2adabd849eabf6f223bfba4bb17
BennyOe <45036971+BennyOe@users.noreply.github.com>
2021-07-03 17:34:25 +0200

    Update gui.sh

--- gui.sh
+++ gui.sh
@@ -380,7 +380,7 @@ clear
 printf "Installing vim\n"
 sleep 2
 
-yay -S --noconfirm neovim-nightly-bin
+yay -S --noconfirm neovim
 
 #Vim-Plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
