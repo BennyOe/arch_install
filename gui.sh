8afe280f4d71af2b00d2e8544086930386bfe477
Benny <45036971+BennyOe@users.noreply.github.com>
2022-01-24 18:03:01 +0100

    Update gui.sh

--- gui.sh
+++ gui.sh
@@ -2,7 +2,7 @@
 ###########################
 #### Array Definitions ####
 ###########################
-systemApps=(pacman-contrib archlinux-contrib sysstat ttf-font-awesome ttf-symbola dmenu network-manager-applet networkmanager-openconnect gnu-free-fonts zsh papirus-icon-theme gtk4 arc-gtk-theme arandr lxappearance timeshift grub-customizer polkit polkit-gnome feh bluez bluez-utils blueman viewnior xcape multilockscreen gotop cifs-utils ntfs-3g xclip udisks thunar-volman pulseaudio-bluetooth pamixer gvfs xfce4-settings bat ripgrep fd)
+systemApps=(pacman-contrib archlinux-contrib sysstat ttf-font-awesome ttf-symbola dmenu network-manager-applet networkmanager-openconnect gnu-free-fonts zsh papirus-icon-theme gtk4 arc-gtk-theme arandr lxappearance timeshift grub-customizer polkit polkit-gnome feh bluez bluez-utils blueman viewnior xcape multilockscreen gotop cifs-utils ntfs-3g xclip udisks thunar-volman pulseaudio-bluetooth pamixer gvfs xfce4-settings bat ripgrep fd seahorse)
 essentialApps=(pulseaudio pulseaudio-alsa pavucontrol pa-applet-git ponymix ranger redshift thunar thunar-volman numlockx zathura htop-vim-git neofetch nodejs npm python-pynvim xarchiver unzip ueberzug zathura-pdf-mupdf lazygit zip-3.0-9 xdotool nerd-fonts-jetbrains-mono)
 
 ###########################
