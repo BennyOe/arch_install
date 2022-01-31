ef8e5c6a95cf564239ae818ecfbd4e9524cf63ae
Benny <45036971+BennyOe@users.noreply.github.com>
2022-01-31 17:11:53 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 curl -sL https://git.io/J1M42 > /etc/pacman.conf
-pacman -Sy && pacman-key --refresh-keys --noconfirm dialog
+pacman -Sy --noconfirm dialog
 
 #################
 #### Welcome ####
